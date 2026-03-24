bceaeca35952455c9fe786f7303489f6c2be4af0
GuilhermeRech08 <guilherme_s_rech@estudante.sesisenai.org.br>
2026-03-24 10:00:06 -0300

    resolvendo alguns bugs e removendo coisas

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 let des = document.getElementById('des').getContext('2d')
 
-let carroInimigo = new CarroInimigo(1300, 325, 80, 50, './img/obstaculo.png')
-let carroInimigo2 = new CarroInimigo(1500, 125, 80, 50, './img/obstaculo.png')
-let carroInimigo3 = new CarroInimigo(1700, 400, 80, 50, './img/obstaculo.png')
-let carro = new Carro(100, 325, 80, 50, '../img/skiador.png')
+let carroInimigo = new CarroInimigo(1300, 325, 100, 100, './img/obstaculo.png')
+let carroInimigo2 = new CarroInimigo(1500, 125, 100, 100, './img/obstaculo.png')
+let carroInimigo3 = new CarroInimigo(1700, 400, 100, 100, './img/obstaculo.png')
+let carroInimigo4 = new CarroInimigo(1900, 450, 100, 100, './img/obstaculo.png')
+let carroInimigo5 = new CarroInimigo(2100, 205, 100, 100, './img/obstaculo.png')
+let carro = new Carro(100, 325, 75, 75, '../img/skiador.png')
 // let medidaCarro = new Carro(100, 325, 85, 50, 'green')
 
 let t1 = new Text()
 let t2 = new Text()
 let fase_txt = new Text()
 
-let motor = new Audio('./img/motor.wav')
-let batida = new Audio('./img/batida.mp3')
-motor.volume = 0.5
-motor.loop = true
-batida.volume = 0.5
-
 let jogar = true
 let fase = 1
 
 document.addEventListener('keydown', (e) => {
-    motor.play()
     if (e.key === 'w' || e.key === 'ArrowUp') {
         carro.dir = -10
     } else if (e.key === 's' || e.key === 'ArrowDown') {
         carro.dir = +10
     }
+    console.log(e.key)
 })
 
 document.addEventListener('keyup', (e) => {
@@ -34,12 +30,13 @@ document.addEventListener('keyup', (e) => {
     } else if (e.key === 's' || e.key === 'ArrowDown') {
         carro.dir = 0
     }
+
+    console.log(e.key)
 })
 
 function game_over() {
     if (carro.vida <= 0) {
         jogar = false
-        motor.pause()
         // música com o jogo parado
     }
 }
@@ -50,29 +47,38 @@ function ver_fase() {
         carroInimigo.vel = 8
         carroInimigo2.vel = 8
         carroInimigo3.vel = 8
+        carroInimigo4.vel = 8
+        carroInimigo5.vel = 8
     } else if (carro.pontos > 200 && fase === 2) {
         fase = 3
         carroInimigo.vel = 12
         carroInimigo2.vel = 12
         carroInimigo3.vel = 12
+        carroInimigo4.vel = 12
+        carroInimigo5.vel = 12
     }
 }
 
 function colisao() {
     if (carro.colid(carroInimigo)) {
-        batida.play()
         carroInimigo.recomeca()
         carro.vida -= 1
 
     }
     if (carro.colid(carroInimigo2)) {
-        batida.play()
         carroInimigo2.recomeca()
         carro.vida -= 1
     }
     if (carro.colid(carroInimigo3)) {
-        batida.play()
         carroInimigo3.recomeca()
+        carro.vida -= 1
+    }
+    if (carro.colid(carroInimigo4)) {
+        carroInimigo4.recomeca()
+        carro.vida -= 1
+    }
+    if (carro.colid(carroInimigo5)) {
+        carroInimigo5.recomeca()
         carro.vida -= 1
     }
     console.log('vida: ', carro.vida)
@@ -91,6 +97,14 @@ function pontuacao() {
         carro.pontos += 10
         carroInimigo3.recomeca()
     }
+    if (carro.point(carroInimigo4)) {
+        carro.pontos += 10
+        carroInimigo4.recomeca()
+    }
+    if (carro.point(carroInimigo5)) {
+        carro.pontos += 10
+        carroInimigo5.recomeca()
+    }
 }
 
 function desenha() {
@@ -99,6 +113,8 @@ function desenha() {
         carroInimigo.des_carro()
         carroInimigo2.des_carro()
         carroInimigo3.des_carro()
+        carroInimigo4.des_carro()
+        carroInimigo5.des_carro()
         carro.des_carro()
         t1.des_text('Pontos: ' + carro.pontos, 1000, 40, 'yellow', '26px Arial')
         t2.des_text('Vidas: ' + carro.vida, 40, 40, 'red', '26px Arial')
@@ -117,6 +133,8 @@ function atualiza() {
         carroInimigo.mov_car()
         carroInimigo2.mov_car()
         carroInimigo3.mov_car()
+        carroInimigo4.mov_car()
+        carroInimigo5.mov_car()
         colisao()
         pontuacao()
         ver_fase()
